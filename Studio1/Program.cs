dafd4ed0a5cefc99a5b3c8d06f664d613750fbfe
cjconnor18 <cjallen1988@gmail.com>
2020-08-31 15:26:35 -0500

    created circumference and diameter of circle

---
+++ Studio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Studio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Radius: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = Math.Round(Math.PI * Math.Pow(radius, 2), 3);
+            Console.WriteLine("The area is of a circle with a raius of " + radius + " is: " + area);
+
+            double circumference = Math.Round(2 * Math.PI * radius, 3);
+            double diameter = Math.Round(2 * radius, 3);
+
+            Console.WriteLine("The circumference is: " + circumference);
+            Console.WriteLine("The diameter is: " + diameter);
+        }
+    }
+}
